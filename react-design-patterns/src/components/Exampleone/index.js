82f6d315c754a68f53d91dbfd4cbdb0889783f19
Daniel Khan Hafner <danielkhanhafner@gmail.com>
2021-04-15 10:34:15 -0400

    Daniel, memento pattern example

--- react-design-patterns/src/components/Exampleone/index.js
+++ react-design-patterns/src/components/Exampleone/index.js
@@ -1,14 +1,46 @@
 import React, {Component, useEffect, useRef, useState} from 'react';
+import {useInput} from './useInput.js';
 
 import './style.scss';
 
+var savedState = []
+
+const createMemento = (e, userIn) => {
+  savedState.push(userIn)
+  alert("Creating Memento with state: " + savedState[savedState.length - 1])
+  e.preventDefault()
+}
+
+const restoreMemento = (e) => {
+  alert("Restoring from Memento: " + savedState[savedState.length - 1])
+  e.preventDefault()
+}
 
 const Exampleone = () => {
 
+  const { value:userIn, bind:bindUserIn, reset:resetUserIn } = useInput('')
+
   return(<div>
 
-    <h1>Example One</h1>
-
+    <div>
+      <h1>Memento Example:</h1>
+      &nbsp;
+    </div>
+    
+    <form>
+      <label>
+        Enter Text Here:&nbsp;
+        <textarea type="text" name="userIn" {...bindUserIn} required/>
+      </label>
+      &nbsp;
+      <button className="btn" onClick={e => createMemento(e, userIn)}>
+        Create Memento
+      </button>
+      &nbsp;
+      <button className="btn" onClick={e => {restoreMemento(e); resetUserIn(savedState.pop());}}>
+        Restore Memento
+      </button>
+    </form>
 
     </div>)
 }
